d09b6e915f0e88a62d1e8c1c5c330bb7a5132732
balaji89 <reddybalaji162@gmail.com>
2015-11-25 14:08:09 -0800

    added scss files

--- js/script.js
+++ js/script.js
@@ -1,53 +1,91 @@
-$(function() {
-var albumData,
-albumItems,
-artistName,
-itunesUrl,
-$albumList = $('.album-list');
-$('#album-search').on('submit', function(event) {
-$('#searchtags').hide();
-event.preventDefault();
-$albumList.empty();
-albumData, albumItems = '',
-artistName = $('#artist-name').val().replace(/ /g, '+'),
-itunesUrl = 'https://api.instagram.com/v1/tags/'+artistName+'/media/recent?count=12&client_id=fed3ea7d36644c4993d81a12621c74c8';
-$.ajax({
-method: 'GET',
-url: itunesUrl,
-dataType: 'jsonp'
-})
-.done(function(results) {
-albumData = results.data;
-if ( albumData.length !== 0 ) {
-$.each(albumData, function(key, value) {
-albumItems += '<div class= "pics">';
-albumItems += '<img src="' + value.images.standard_resolution.url + '" />';
-albumItems += '<div class="container">';
-albumItems += '<div class="profile">';
-albumItems += '<img src="' + value.caption.from.profile_picture + '" />';
-albumItems += '</div>';
-albumItems += '<ul class="tags">';
-albumItems +='<li>';
-albumItems += '<p>' + value.caption.from.username + '</p>';
-albumItems += '<p>'
-albumItems += '<i class="fa fa-heart"></i> ' +value.likes.count+ ' ';
-albumItems += '<i class="fa fa-comments"></i> ' +value.comments.count;
-albumItems += '</p>'
-albumItems += '</li>';
-albumItems += '</ul>';
-albumItems += '</div>';
-albumItems += '</div>';
-});
-} else {
-albumItems += '<p style="margin-top: 18px;">Sorry, artist not found.</p>';
-}
-$albumList.append(albumItems);
-})
-    // and if it fails...
-    .fail(function() {
-       $albumList.append('<li>Sorry! There was a problem, please try again.</li>');
-    });
- });
-});
+(function ($) {
+	"use strict";
 
+	/**
+	 * Ajax request to Instagaram API
+	 */
 
+	$(function() {
+
+		// set some initial variables
+		var albumData,
+ 			albumItems,
+ 			searchTerm,
+ 			endpoint,
+			$albumList = $('.album-list');
+			// $loadMore = $('.load-button');
+ 			$('.search-form').on('submit', function(event) {
+ 			$('#searchtags').hide();
+
+ 			event.preventDefault();
+
+ 			// reset all the things
+ 			$albumList.empty();
+ 			albumData = ''; 
+ 			albumItems = '';
+
+ 			// get the search string
+ 			searchTerm = $('#hash-tag').val().replace(/ /g, '+'),
+		 	endpoint = 'https://api.instagram.com/v1/tags/'+searchTerm+'/media/recent?count=12&client_id=fed3ea7d36644c4993d81a12621c74c8';
+			
+			// make the call to the endpoint
+			$.ajax({
+ 				method: 'GET',
+				url: endpoint,
+ 				dataType: 'jsonp'
+ 			}).done(function(results) {
+
+ 				albumData = results.data;
+
+ 				// append the photos if we found any
+ 				if ( albumData.length !== 0 ) {
+
+ 					albumItems += '<ul>';
+
+ 				$.each(albumData, function(key, value) {
+ 								albumItems += '<li>';
+ 								albumItems += '<div class= "inner-item-wrapper">';
+ 								albumItems += '<img src="' + value.images.standard_resolution.url + '" />';
+ 							
+ 								albumItems += '<div class="photo-meta">';
+ 								albumItems += '<div class="profile">';
+ 								albumItems += '<img src="' + value.caption.from.profile_picture + '" />';
+ 								albumItems += '</div>';
+ 								albumItems += '<div class="social">';
+ 								albumItems += '<p>' + value.caption.from.username + '</p>';
+ 								albumItems += '<p>'
+ 								albumItems += '<i class="fa fa-heart"></i> ' +value.likes.count+ ' ';
+ 								albumItems += '<i class="fa fa-comments"></i> ' +value.comments.count;
+ 								albumItems += '</p>'
+ 								albumItems += '</div>';
+ 								albumItems += '</div>';
+ 								albumItems += '</div>';
+ 								albumItems += '</li>';
+	});
+							albumItems += '</ul>';
+
+ } else {
+ 	albumItems += '<p class="feedback">Sorry, nothing found! Please try again.</p>';
+ 
+
+ }
+
+ 			$albumList.append(albumItems);
+ 
+     // and if it fails...
+
+    		}).fail(function() {
+
+        		$albumList.append('<p class="feedback">Sorry! There was a problem, please try again.</p>');
+
+    		}).always (function() {
+
+    			$loader.hide();
+
+    	 	})
+
+  		});
+
+	});
+
+}(jQuery));
